091e07b2113cf197659854d1d187c8d892c4a0d3
Weifang Chen <weifang.chen@oracle.com>
2017-06-12 10:51:43 +0800

    add like page

--- my-miniapp-code/demo-1/pages/interact/interact.js
+++ my-miniapp-code/demo-1/pages/interact/interact.js
@@ -5,8 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
+    x: 60,
+    y: 60,
+    r: 40,
     likeCount: 20,
-    unlikeCount: 15,
+    unlikeCount: 20,
     likeColor: 'green',
     unlikeColor: 'grey',
     iconSize: [20, 30, 40, 50, 60, 70],
@@ -22,21 +25,28 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var percentage = this.data.likeCount / (this.data.likeCount + this.data.unlikeCount);
+    var perStr = Math.round(percentage * 100) + '%'
     // 页面渲染完成 
     var cxt_arc = wx.createCanvasContext('canvasArc');//创建并返回绘图上下文context对象。 
-    cxt_arc.setLineWidth(6);
+    cxt_arc.setLineWidth(2);
     cxt_arc.setStrokeStyle('#d2d2d2');
     cxt_arc.setLineCap('round')
     cxt_arc.beginPath();//开始一个新的路径 
-    cxt_arc.arc(106, 106, 100, 0, 2 * Math.PI, false);//设置一个原点(106,106)，半径为100的圆的路径到当前路径 
+    cxt_arc.arc(this.data.x, this.data.y, this.data.r, 0, 2 * Math.PI, false);//设置一个原点(106,106)，半径为50的圆的路径到当前路径 
     cxt_arc.stroke();//对当前路径进行描边 
 
-    cxt_arc.setLineWidth(6);
-    cxt_arc.setStrokeStyle('#3ea6ff');
+    cxt_arc.setLineWidth(2);
+    cxt_arc.setStrokeStyle('#76a915');
     cxt_arc.setLineCap('round')
     cxt_arc.beginPath();//开始一个新的路径 
-    cxt_arc.arc(106, 106, 100, -Math.PI * 1 / 2, Math.PI * 6 / 5, false);
+    cxt_arc.arc(this.data.x, this.data.y, this.data.r, -Math.PI * 1 / 2, Math.PI * percentage, false);
     cxt_arc.stroke();//对当前路径进行描边 
+
+    cxt_arc.setFontSize(20)
+    cxt_arc.setTextAlign('center')
+    cxt_arc.fillText(perStr, this.data.x, this.data.y+5)
+    cxt_arc.stroke();
 
     cxt_arc.draw();
   },
